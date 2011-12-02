0c08eb81deac9b2748bd79765294bf4b06964f1c
tomnz <tomnz@localhost>
2011-12-01 17:27:48 -0800

    Improvements: - Refactored and cleaned up Update/Gravity code New features: - Added initial Thruster class for thruster physics

---
+++ MangoLander/MangoLander/Physics/Thruster.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace MangoLander.Physics
+{
+    class Thruster
+    {
+        // Constants
+        private const float _DEFAULT_STRENGTH = 60;
+
+        // Properties
+        public float Strength { get; set; }
+        public bool Active { get; set; }
+
+        // Constructors
+        public Thruster()
+        {
+            this.Strength = _DEFAULT_STRENGTH;
+            this.Active = false;
+        }
+
+        public Thruster(float strength) :
+            this()
+        {
+            this.Strength = strength;
+        }
+
+        // Methods
+        public Vector2 GetAcceleration()
+        {
+            if (!this.Active)
+                return new Vector2();
+
+            Vector2 result = new Vector2(0, - this.Strength);
+            return result;
+        }
+    }
+}
